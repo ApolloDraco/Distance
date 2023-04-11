f665a52615e6d5f4bdb5ee8bf7e8102b6c69a137
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-11 17:31:02 -0400

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -3,24 +3,26 @@
 */
 
 import java.util.Scanner;
+
 public class Distance {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args){
-        
-        Scanner in = new Scanner(System.in);
-
+        // Prompt the user to enter the two points
         System.out.print("Enter x1 and y1: ");
-        double x1 = in.nextDouble();
-        double y1 = in.nextDouble();
-        
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+
         System.out.print("Enter x2 and y2: ");
-        double x2 = in.nextDouble();
-        double y2 = in.nextDouble();
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
 
-        double determinant = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
-        double sqrRoot = Math.pow(determinant, 0.5);
+        // Calculate the distance between the two points
+        double distanceSquared = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2);
+        double distance = Math.pow(distanceSquared, 0.5);
 
-        System.out.printf("The distance between two points is %f", sqrRoot);
-
+        // Display the result
+        System.out.printf("The distance between the two points is %.2f", distance);
     }
 }
